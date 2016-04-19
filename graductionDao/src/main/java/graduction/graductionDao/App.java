857ca0ebe4261426340c43c8e76f81a433ae3c11
Smile_Bean <yxw1280355133@gmail.com>
2016-04-19 14:50:54 +0800

    initial commit

---
+++ graductionDao/src/main/java/graduction/graductionDao/App.java
@@ -0,0 +1,13 @@
+package graduction.graductionDao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
